c85f1a27a3d3b0b29c9cead30b5566e08b407a4f
AbenetGebre <mail.micrq@proton.me>
2024-08-05 23:27:47 +0300

    documentation added for all function and classes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,11 +5,15 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """ template for all authentications """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ used to require authentication """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ used to get the authorization header """
         return None
     
     def current_user(self, request=None) -> TypeVar('User'):
+        """ used to get the current user """
         return None
