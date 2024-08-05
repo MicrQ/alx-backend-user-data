ce19e2a321316fd360f2bad27c2d2e67f902db31
AbenetGebre <mail.micrq@proton.me>
2024-08-05 23:43:31 +0300

    modified require_auth method to check if routes require auhentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,12 @@ class Auth:
     """ template for all authentications """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ used to require authentication """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        path1, path2 = (path[:-1], path) if path[-1] == '/' else (path, path + '/')
+        if path1 in excluded_paths or path2 in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ used to get the authorization header """
