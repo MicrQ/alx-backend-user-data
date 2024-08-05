5665d78be2f7747c360895f121fafa1ccfd1c02c
AbenetGebre <mail.micrq@proton.me>
2024-08-05 23:11:55 +0300

    forbidden error page created

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> str:
     Used to check unauthorized error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Used to check forbidden error
+    """
+    abort(403)
